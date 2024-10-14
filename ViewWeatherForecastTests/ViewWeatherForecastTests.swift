361fc623edfe6a64cb36983477c7391e1b74f11f
Jason <jemanuel@student.ciputra.ac.id>
2024-10-14 13:16:16 +0700

    Initial Commit

---
+++ ViewWeatherForecastTests/ViewWeatherForecastTests.swift
@@ -0,0 +1,17 @@
+//
+//  ViewWeatherForecastTests.swift
+//  ViewWeatherForecastTests
+//
+//  Created by Jason Emanuel on 14/10/24.
+//
+
+import Testing
+@testable import ViewWeatherForecast
+
+struct ViewWeatherForecastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
